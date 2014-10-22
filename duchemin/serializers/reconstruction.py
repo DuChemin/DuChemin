8d7cd7b9e48fb43e688b0d8de4c75cf3ffea9175
Andrew Hankinson <andrew.hankinson@gmail.com>
2014-10-22 13:22:06 -0400

    Fixing reconstruction page view

--- duchemin/serializers/reconstruction.py
+++ duchemin/serializers/reconstruction.py
@@ -1,7 +1,28 @@
 from duchemin.models.reconstruction import DCReconstruction
+from duchemin.models.piece import DCPiece
+from duchemin.models.person import DCPerson
 from rest_framework import serializers
 
 
+class DCReconstructionPieceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = DCPiece
+        lookup_field = "piece_id"
+        fields = ('url', 'title', 'piece_id')
+
+
+class DCReconstructionPersonSerializer(serializers.HyperlinkedModelSerializer):
+    full_name = serializers.Field(source="full_name")
+
+    class Meta:
+        model = DCPerson
+        lookup_field = 'person_id'
+        fields = ('url', 'full_name')
+
 class DCReconstructionSerializer(serializers.HyperlinkedModelSerializer):
+    piece = DCReconstructionPieceSerializer()
+    reconstructor = DCReconstructionPersonSerializer()
+
     class Meta:
         model = DCReconstruction
+        fields = ('url', 'piece', 'reconstructor')
