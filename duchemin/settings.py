12f48fc3f42afd42fdef634a13035c67b7cfd95f
root <admin@digitalduchemin.org>
2024-10-28 18:57:30 +0000

    Commit version working with Caddy

--- duchemin/settings.py
+++ duchemin/settings.py
@@ -9,7 +9,7 @@ ALLOWED_HOSTS = [
     '.localhost',
     '[::1]',
     'dev.digitalduchemin.org',
-    'dev.duchemin.mdap-testing.com',
+    'dev2.duchemin.mdap-testing.com',
 ]
 
 PROJECT_DIR = os.path.dirname(__file__)
@@ -76,7 +76,7 @@ MP3_ROOT = os.path.join(PROJECT_DIR, 'uploads/audio')
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'collected'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -7,14 +7,14 @@ cd /app
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
 # Apply migrations and run the Django development server
-# python manage.py loaddata /app/duchemin/data/duchemin_dcperson.json
-# python manage.py loaddata /app/duchemin/data/duchemin_dcbook.json
-# python manage.py loaddata /app/duchemin/data/duchemin_dccontentblock.json
-# python manage.py loaddata /app/duchemin/data/duchemin_dcpiece.json
-# python manage.py loaddata /app/duchemin/data/duchemin_dcreconstruction.json
-# python manage.py loaddata /app/duchemin/data/duchemin_dcphrase.json
-# python manage.py loaddata /app/duchemin/data/duchemin_dcanalysis.json
-# python manage.py loaddata /app/duchemin/data/duchemin_dcnote.json
+#python manage.py loaddata /app/duchemin/data/duchemin_dcperson.json
+#python manage.py loaddata /app/duchemin/data/duchemin_dcbook.json
+#python manage.py loaddata /app/duchemin/data/duchemin_dccontentblock.json
+#python manage.py loaddata /app/duchemin/data/duchemin_dcpiece.json
+#python manage.py loaddata /app/duchemin/data/duchemin_dcreconstruction.json
+#python manage.py loaddata /app/duchemin/data/duchemin_dcphrase.json
+#python manage.py loaddata /app/duchemin/data/duchemin_dcanalysis.json
+#python manage.py loaddata /app/duchemin/data/duchemin_dcnote.json
