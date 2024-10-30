34ff848afe16103223e2eb38257df39153616cbc
root <admin@digitalduchemin.org>
2024-10-30 17:01:44 +0000

    Update figuredash template tag

--- duchemin/templatetags/figuredash.py
+++ duchemin/templatetags/figuredash.py
@@ -1,6 +1,5 @@
 from django.template.defaultfilters import register
 
-
 @register.filter(name='figuredash')
 def figuredash(string):
-    return unicode(string).replace(u"-", u"\u2013")
+    return str(string).replace("-", "\u2013")
