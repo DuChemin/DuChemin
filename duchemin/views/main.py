cdf7168c247d5b55c7256f5864a61d38b33022d9
root <admin@digitalduchemin.org>
2024-11-01 00:17:30 +0000

    Import reverse_lazy

--- duchemin/views/main.py
+++ duchemin/views/main.py
@@ -1,5 +1,6 @@
 import datetime
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
