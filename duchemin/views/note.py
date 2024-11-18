69f060fd7ffa73841db340807cdfa85666f5dafe
root <admin@digitalduchemin.org>
2024-11-18 22:47:26 +0000

    Updates to notes view

--- duchemin/views/note.py
+++ duchemin/views/note.py
@@ -1,12 +1,7 @@
-from urllib.parse import urlparse
-from rest_framework import generics
+from rest_framework import generics, permissions, status
 from rest_framework.renderers import JSONRenderer
-from rest_framework import permissions
-from rest_framework import status
 from rest_framework.response import Response
-
 from django.contrib.auth.models import User
-from django.urls import resolve
 from django.shortcuts import get_object_or_404
 from django.http import Http404
 
@@ -16,66 +11,40 @@ from duchemin.serializers.note import DCNoteSerializer
 
 
 class NoteList(generics.ListCreateAPIView):
-    model = DCNote
     serializer_class = DCNoteSerializer
     permission_classes = (permissions.IsAuthenticated,)
     renderer_classes = (JSONRenderer,)
-    paginate_by = 100
-    paginate_by_param = 'page_size'
-    max_paginate_by = 200
 
     def get_queryset(self):
-        current_user = User.objects.get(pk=self.request.user.id)
-
-        # Must filter by author to prevent making everyone's notes public
-        queryset = DCNote.objects.filter(author=current_user)
-
-        return queryset
+        current_user = self.request.user
+        return DCNote.objects.filter(author=current_user)
 
     def post(self, request, *args, **kwargs):
-        piece_id = request.DATA.get('piece_id', None)
-        note_text = request.DATA.get('text', None)
+        piece_id = request.data.get('piece_id', None)
+        note_text = request.data.get('text', None)
 
         piece_obj = get_object_or_404(DCPiece, piece_id=piece_id)
+        current_user = request.user
 
-        current_user = User.objects.get(pk=request.user.id)
-        note = DCNote()
-        note.piece = piece_obj
-        note.author = current_user
-        note.text = note_text
-
-        # Since `current_user` was gotten from the login, we're not
-        # letting just anybody write anywhere.
+        note = DCNote(piece=piece_obj, author=current_user, text=note_text)
         note.save()
 
         serialized = DCNoteSerializer(note).data
-
         return Response(serialized, status=status.HTTP_201_CREATED)
 
 
 class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
-    model = DCNote
     serializer_class = DCNoteSerializer
     permission_classes = (permissions.IsAuthenticated,)
     renderer_classes = (JSONRenderer,)
-    paginate_by = 100
-    paginate_by_param = 'page_size'
-    max_paginate_by = 200
-    slug_field="piece"
 
     def get_object(self):
-        try:
-            current_user = User.objects.get(pk=self.request.user.id)
-        	# Get the most recently updated note associated with our piece_id
-            # out of the set of this user's notes
-            return DCNote.objects.filter(author=current_user,piece__piece_id=self.kwargs["pk"]).order_by("-updated")[0]
-        except IndexError:
+        current_user = self.request.user
+        note = DCNote.objects.filter(author=current_user, piece__piece_id=self.kwargs["pk"]).order_by("-updated").first()
+        if not note:
             raise Http404
+        return note
 
     def get_queryset(self):
-        current_user = User.objects.get(pk=self.request.user.id)
-
-        # Must filter by author to prevent making everyone's notes public
-        queryset = DCNote.objects.filter(author=current_user)
-
-        return queryset
+        current_user = self.request.user
+        return DCNote.objects.filter(author=current_user)
